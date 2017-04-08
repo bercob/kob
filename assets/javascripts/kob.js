891a3ea2f47076b5bcd60d61949e0199371cb125
berco <bercob@gmail.com>
2017-04-08 23:47:47 +0200

    finish all section - beta version

---
+++ assets/javascripts/kob.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+	var owl = $('.owl-carousel');
+  	owl.owlCarousel({
+      items: 1,
+      loop: true,
+      margin: 10,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      autoplayHoverPause: true
+  	});
+
+  	$('span.detail-down').click(function () {
+  		slideToggleDetail($(this), $(this).parent().siblings('.section-detail'));
+  	})
+
+$('span.detail-up').click(function () {
+  		slideToggleDetail($(this).parent().parent().parent().find('span.detail-down'), $(this).parent().parent('.section-detail'));
+  	})      	
+
+})
+
+function slideToggleDetail(button, detail) {
+	if (detail.is(':visible')) {
+		button.removeClass('glyphicon-chevron-up');
+		button.addClass('glyphicon-chevron-down');
+	} else {
+		button.removeClass('glyphicon-chevron-down');
+		button.addClass('glyphicon-chevron-up');
+	}  		
+	detail.slideToggle();
+}
