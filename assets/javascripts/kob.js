350e3af624f49d1173688da289575151270156c6
berco <bercob@gmail.com>
2017-04-09 00:15:03 +0200

    fix collapsing hamburger menu

--- assets/javascripts/kob.js
+++ assets/javascripts/kob.js
@@ -1,21 +1,25 @@
 $(document).ready(function() {
 	var owl = $('.owl-carousel');
-  	owl.owlCarousel({
-      items: 1,
-      loop: true,
-      margin: 10,
-      autoplay: true,
-      autoplayTimeout: 3000,
-      autoplayHoverPause: true
-  	});
+	owl.owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true
+	});
 
-  	$('span.detail-down').click(function () {
-  		slideToggleDetail($(this), $(this).parent().siblings('.section-detail'));
-  	})
+	$('span.detail-down').click(function () {
+		slideToggleDetail($(this), $(this).parent().siblings('.section-detail'));
+	})
 
-$('span.detail-up').click(function () {
-  		slideToggleDetail($(this).parent().parent().parent().find('span.detail-down'), $(this).parent().parent('.section-detail'));
-  	})      	
+  $('span.detail-up').click(function () {
+		slideToggleDetail($(this).parent().parent().parent().find('span.detail-down'), $(this).parent().parent('.section-detail'));
+	})
+
+  $('.navbar-nav li a').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  })
 
 })
 
